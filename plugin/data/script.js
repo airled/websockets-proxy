82ab1ad09a1c555aaa900c17b21f867ea3696cce
airled <airled@mail.ru>
2016-01-19 09:59:39 +0300

    notifications and icons

--- plugin/data/script.js
+++ plugin/data/script.js
@@ -1,14 +1,16 @@
 var ws = new WebSocket('ws://localhost:4567');
 ws.onopen = function(){
 	console.log('websocket opened');
+    self.port.emit('notificate', 'Websocket opened');
 };
-ws.onclose = function(){
-	console.log('websocket closed');
+ws.onclose = function() {
+    console.log('websocket closed');
+	self.port.emit('notificate', 'Websocket closed');
 };
 ws.onmessage = function(message) {
 	self.port.emit('message', message.data);
 };
 
-self.port.on('response', function(response){
+self.port.on('response', function(response) {
 	ws.send(response);
 });
--- plugin/index.js
+++ plugin/index.js
@@ -1,14 +1,52 @@
-var panel = require("sdk/panel").Panel({
-  contentScriptFile: "./script.js"
+var { ActionButton } = require("sdk/ui/button/action");
+
+var button = ActionButton({
+    id: "Websocket",
+    label: "Open websocket",
+    icon: {
+      "16": "./16.png",
+      "32": "./32.png",
+      "64": "./64.png"
+    },
+    onClick: function(state) {
+        var panel = require("sdk/panel").Panel({
+            contentScriptFile: "./script.js"
+        });
+        
+        panel.port.on('message', function(message){
+            var request = require("sdk/request").Request({
+                url: message,
+                onComplete: function (response) {
+                    panel.port.emit('response', response.text)
+                }
+            });
+            request.get();
+        });
+
+        panel.port.on('notificate', function(message){
+            var notification = require("sdk/notifications").notify({
+                title: message
+            });
+        });
+    }
 });
 
-panel.port.on('message', function(message){
-  var request = require("sdk/request").Request({
-    url: message,
-    overrideMimeType: "text/plain; charset=latin1",
-    onComplete: function (response) {
-      panel.port.emit('response', response.text)
-    }
-  });
-  request.get();
-});
+// var panel = require("sdk/panel").Panel({
+//     contentScriptFile: "./script.js"
+// });
+
+// panel.port.on('message', function(message){
+//     var request = require("sdk/request").Request({
+//         url: message,
+//         onComplete: function (response) {
+//             panel.port.emit('response', response.text)
+//         }
+//     });
+//     request.get();
+// });
+
+// panel.port.on('notificate', function(message){
+//     var notification = require("sdk/notifications").notify({
+//         title: message
+//     });
+// });
