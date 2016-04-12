9a6d6644e7bc660c2dce1872a222be6b84024ce0
airled <airled@mail.ru>
2016-04-12 21:54:08 +0300

    plugin tests fixed

--- firefox-plugin/test/test-prefsHandler.js
+++ firefox-plugin/test/test-prefsHandler.js
@@ -7,12 +7,16 @@ exports["test fetch preferences"] = function(assert) {
   preferences.prefs["Password"] = "password123";
   preferences.prefs["Proxy address"] = "http://test.com:9999";
   preferences.prefs["Reconnection timeout"] = "20";
+  preferences.prefs["Profile"] = "wsprofile";
+  preferences.prefs["Proxy profile"] = "proxyprofile";
   var prefs = handler.fetch();
   assert.ok(prefs.wsaddress === "ws://test.com:1234");
   assert.ok(prefs.email === "test@test.com");
   assert.ok(prefs.password === "password123");
   assert.ok(prefs.proxyaddress === "http://test.com:9999");
   assert.ok(prefs.timeout === "20");
+  assert.ok(prefs.profile === "wsprofile");
+  assert.ok(prefs.proxyProfile === "proxyprofile");
 }
 
 exports["test save preferences"] = function(assert) {
@@ -22,7 +26,8 @@ exports["test save preferences"] = function(assert) {
     password: "verifyme",
     proxyaddress: "http://thereisno.spoon:9876",
     timeout: "100",
-    profile: "default1"
+    profile: "default1",
+    proxyProfile: "myProxyProfile"
   };
   handler.save(prefs);
   assert.ok(preferences.prefs["Websocket address"] === "ws://testeme.me:4321");
@@ -31,6 +36,7 @@ exports["test save preferences"] = function(assert) {
   assert.ok(preferences.prefs["Proxy address"] === "http://thereisno.spoon:9876");
   assert.ok(preferences.prefs["Reconnection timeout"] === "100");
   assert.ok(preferences.prefs["Profile"] === "default1");
+  assert.ok(preferences.prefs["Proxy profile"] === "myProxyProfile");
 }
 
 require("sdk/test").run(exports);
